67bc9a0c903a8a0059f7180b7ac2c9bcd5761200
JiaminXu1019 <jiaminxu1019@gmail.com>
2018-09-25 09:35:16 -0700

    Share Button added

--- app/src/main/java/com/example/intentslearning/SendMessageActivity.java
+++ app/src/main/java/com/example/intentslearning/SendMessageActivity.java
@@ -11,9 +11,9 @@ public class SendMessageActivity extends AppCompatActivity {
 
     private EditText editText;
     private Button send;
+    private Button buttonShare;
 
     public static final String EXTRA_MESSAGE = "com.example.intentsLearning";
-
 
 
     @Override
@@ -22,18 +22,29 @@ public class SendMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_send_message);
         wireWidgets();
 
+        buttonShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentShare = new Intent(Intent.ACTION_SEND);
+                intentShare.setType("text/plain");
+                intentShare.putExtra(EXTRA_MESSAGE, editText.getText().toString());
+                startActivity(intentShare);
+            }
+        });
     }
 
-    private void wireWidgets() {
-        editText = findViewById(R.id.edittext_sendmessage_msg);
-        send = findViewById(R.id.button_sendmessage_msg);
-    }
 
-    public void sendMessage(View view) {
-        String message = editText.getText().toString();
+            private void wireWidgets() {
+                editText = findViewById(R.id.edittext_sendmessage_msg);
+                send = findViewById(R.id.button_sendmessage_msg);
+                buttonShare = findViewById(R.id.button_share_msg);
+            }
 
-        Intent intent = new Intent(this, DisplayMessage.class);
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-}
+            public void sendMessage(View view) {
+                String message = editText.getText().toString();
+
+                Intent intent = new Intent(this, DisplayMessage.class);
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+            }
+        }
